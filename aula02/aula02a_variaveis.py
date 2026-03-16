7d93141566b493e721eb69ef92f4d0c32e7a59b7
araujob7357.code <araujob7357@gmail.com>
2026-03-16 09:53:16 -0300

    aula02 - variáveis

---
+++ aula02/aula02a_variaveis.py
@@ -0,0 +1,24 @@
+print('Olá mundo!')
+print(7+4)
+print('7'+"4") #concatenando strings
+
+'''
+comentários 
+de 
+múltiplas 
+linhas 
+'''
+
+nome = 'bia'
+idade = '17'
+peso = '50.5'
+print (nome, idade, peso)
+
+# input: receber dados do usuário, o usuário tem a chance de escrever
+
+nome = input (" Digite seu nome: ")
+idade = input ("Digite sua idade: ")
+peso = input ("Digite seu peso: ")
+
+print (nome, idade, peso)
+
---
+++ aula02/desafios.py
@@ -0,0 +1,12 @@
+nome = input ("Digite seu nome: ")
+print(f"Seja bem vindo {nome}")
+
+dia = input ("Digite o dia do seu nascimento:")
+mes = input ("Digite o mês do seu nascimento:")
+ano = input ("Digite o ano do seu nascimento:")
+print (f"Seus dados do nascimento: {dia}/{mes}/{ano}")
+
+numero_um = input ("Digite um número aleatório:")
+numero_dois = input ("Digite um segundo número aleatório:")
+print (numero_um + numero_dois)
+
